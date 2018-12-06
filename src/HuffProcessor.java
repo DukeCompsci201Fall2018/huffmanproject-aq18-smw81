97602a1760dae5278de175f84f5aef00221e5671
1C39 <aqiao1204@gmail.com>
2018-12-06 16:36:18 -0500

    no next line

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -19,16 +19,15 @@ public class HuffProcessor {
 	public static final int PSEUDO_EOF = ALPH_SIZE;
 	public static final int HUFF_NUMBER = 0xface8200;
 	public static final int HUFF_TREE  = HUFF_NUMBER | 1;
-
 	private final int myDebugLevel;
-	
+
 	public static final int DEBUG_HIGH = 4;
 	public static final int DEBUG_LOW = 1;
-	
+
 	public HuffProcessor() {
 		this(0);
 	}
-	
+
 	public HuffProcessor(int debug) {
 		myDebugLevel = debug;
 	}
@@ -48,6 +47,7 @@ public class HuffProcessor {
 			if (val == -1) break;
 			out.writeBits(BITS_PER_WORD, val);
 		}
+		out.close();
 	}
 	/**
 	 * Decompresses a file. Output file must be identical bit-by-bit to the
@@ -60,10 +60,44 @@ public class HuffProcessor {
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
 
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
+		int bits = in.readBits(BITS_PER_INT);
+		if(bits!=HUFF_TREE) throw new HuffException("illegal header starts with " + bits);
+		HuffNode root = readTreeHeader(in);
+		readCompressedBits(root,in,out);
+		out.close();
+	}
+
+	private HuffNode readTreeHeader(BitInputStream in) {
+		int bit = in.readBits(1);
+		if(bit==-1) throw new HuffException("bad input, no PSEUDO_EOF");
+		if(bit==0) {
+			HuffNode left = readTreeHeader(in);
+			HuffNode right = readTreeHeader(in);
+			return new HuffNode(0,0,left,right);
+		}
+		else {
+			int value = in.readBits(BITS_PER_WORD+1);
+			return new HuffNode(value,0,null,null);
+		}
+	}
+
+	private void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out) {
+		HuffNode current = root;
+		while(true) {
+			int bits = in.readBits(1);
+			if(bits == -1) throw new HuffException("bad input, no PSEUDO_EOF");
+			else {
+				if(bits == 0) current = current.myLeft;
+				else current = current.myRight;
+
+				if(current.myLeft == null && current.myRight == null) {
+					if(current.myValue == PSEUDO_EOF) break;
+					else {
+						out.writeBits(BITS_PER_WORD, current.myValue);
+						current = root;
+					}
+				}
+			}
 		}
 	}
 }
