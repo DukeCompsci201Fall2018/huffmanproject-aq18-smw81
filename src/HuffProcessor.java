53a4b559fb335a1c7aab1839b452ad504cc04ec7
Samantha Whitt <smw81@duke.edu>
2018-12-06 23:59:46 -0500

    fail

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -60,12 +60,15 @@ public class HuffProcessor {
 
 	private void writeCopmressedBits(String[] codings, BitInputStream in, BitOutputStream out) {
 		while (true) {
-			if (in.readBits(BITS_PER_WORD) == -1) break;
+			if (in.readBits(BITS_PER_WORD) == -1) {
+				String code = codings[PSEUDO_EOF];
+				out.writeBits(code.length(), Integer.parseInt(code,2));
+			}
 			String code = codings[in.readBits(BITS_PER_WORD)];
 			out.writeBits(code.length(), Integer.parseInt(code,2));
 		}
-		String code = codings[PSEUDO_EOF];
-		out.writeBits(code.length(), Integer.parseInt(code,2));
+		//String code = codings[PSEUDO_EOF];
+		//out.writeBits(code.length(), Integer.parseInt(code,2));
 
 	}
 
